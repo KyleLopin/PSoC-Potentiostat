919375c1c46a2be9ae731145e0ad8fd9bae1a62f
KyleLopin <KyleLopin@gmail.com>
2022-10-26 17:50:32 +0700

    Got first tests working.  Got unittest help function to convert c arrays to list and started a solutions file to store correct answers.

--- py_unit_test/lut_tests/make_lines.py
+++ py_unit_test/lut_tests/make_lines.py
@@ -10,57 +10,68 @@ __author__ = "Kyle Vitautus Lopin"
 import importlib
 import os
 import subprocess
-import uuid
+import unittest
 
 # installed libraries
 import cffi
+
+# local files
+import py_unit_test.helper_functions as helper_funcs
+import solutions
 
 project_dir = os.path.join('..', '..', 'Amperometry_v059_2.cydsn')
 print(project_dir)
 
 
-def preprocess(source, include_paths=[], compiler_options=[]):
-    command = (
-            [
-                "gcc",
-            ]
-            + compiler_options
-            + include_paths
-            + ["-E", "-P", "-"]
-        )
-    print(f"command: {command}")
-    return subprocess.run(command,
-                          input=source, stdout=subprocess.PIPE,
-                          universal_newlines=True, check=True).stdout
-
-
-def load(filename, function_name: str, header_includes=[]):
-    compiled_filename = filename + '_'
-    file_path = os.path.join(project_dir, filename)
-    source = open(file_path+'.c').read()
-    raw_header = open(file_path + '.h').read()
+def load(_filename, function_name: str, header_includes=[]):
+    compiled_filename = _filename + '_'
+    file_path = os.path.join(project_dir, _filename)
+    with open(file_path+'.c', 'r') as fp:
+        source = fp.read()
+    with open(file_path+'.h', 'r') as fp:
+        raw_header = fp.read()
     cdef = ""
 
     for line in raw_header.splitlines():
         if function_name in line:
             cdef += line
-    print('999')
     cdef += "".join(header_includes)
-    print(cdef)
     ffi_builder = cffi.FFI()
     ffi_builder.cdef(cdef)
     ffi_builder.set_source(compiled_filename, source,
                            include_dirs=[project_dir])
     ffi_builder.compile()
 
-    module = importlib.import_module(compiled_filename)
-    return module.lib
+    _module = importlib.import_module(compiled_filename)
+    return _module.lib
 
 
-filename = 'lut_protocols'
-module = load(filename, "LUT_make_line", header_includes=["uint16_t waveform_lut[];"])
-module.LUT_make_line(3, 6, 2)
-print(module.waveform_lut[5])
-print(module.waveform_lut)
-os.remove(filename+'_.c')
+class LuTMakeLineTestCase(unittest.TestCase):
+    def setUp(self):
+        self._filename = 'lut_protocols'
+        self.module = load(self._filename, "LUT_make_line",
+                           header_includes=["uint16_t waveform_lut[];"])
 
+    def tearDown(self) -> None:
+        os.remove(self._filename+'_.c')
+
+    def test1(self):
+        """ Test LUT make line function """
+        index = self.module.LUT_make_line(0, 10, 0)
+        print(f"index: {index}")
+        waveform = helper_funcs.convert_c_array_to_list(self.module.waveform_lut, index)
+        print(f"waveform: {waveform}")
+        self.assertEqual(index, 11)
+        self.assertListEqual(waveform, solutions.test1)
+
+
+# filename = 'lut_protocols'
+# module = load(filename, "LUT_make_line", header_includes=["uint16_t waveform_lut[];"])
+# module.LUT_make_line(3, 6, 2)
+# print(module.waveform_lut[5])
+# print(module.waveform_lut)
+# os.remove(filename+'_.c')
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ py_unit_test/lut_tests/solutions.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022 Kyle Lopin (Naresuan University) <kylel@nu.ac.th>
+
+"""
+For the look-up table (lut) tests, this provides the solution test arrays
+"""
+
+__author__ = "Kyle Vitatus Lopin"
+
+test1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
